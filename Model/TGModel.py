8b680d7232141126508e6624cc340e4f3aad85ea
itought <mountyjapan24@gmail.com>
2025-11-06 13:17:10 +0200

    Iteration in Files

--- Model/TGModel.py
+++ Model/TGModel.py
@@ -8,4 +8,13 @@ class TGModel:
                       "audio":[".mp3", ".wav", ".aiff"],
                       "docs":[".docs", ".docx", ".txt", ".pdf", ".ppt", ".pptx", ".rar", ".zip"]}
 
+    def Analysis(self):
+        files = {l:[] for l in self.types}
 
+        for element in self.path.iterdir():
+            if element.is_file():
+                for key, value in self.types.items():
+                    if element.suffix.lower() in value:
+                        files[key].append(element.name)
+
+        return files
