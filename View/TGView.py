d5fe72a9d68c5f8320f8981a49fe26d4a83686da
EgrNvk <104683701+EgrNvk@users.noreply.github.com>
2025-11-06 13:44:18 +0200

    Add OutputResult

--- View/TGView.py
+++ View/TGView.py
@@ -1,3 +1,10 @@
 class TGView:
     def UserPath(self):
         return input("Введите путь для сохранения: ")
+
+    def OutputResult(self, files, path):
+        print(f"\n{"Результат анализа файлов": ^37}")
+
+        for key, lst in files.items():
+            print(f"{key.capitalize()}:{len(lst)} файлов")
+        print(f"\nФайли сохранены в: {path}")
