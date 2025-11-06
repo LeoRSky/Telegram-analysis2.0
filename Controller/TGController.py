1e7d9d546c2f9586b496bf97c9f692d2373e43d2
LeoR <leorsky77@gmail.com>
2025-11-06 14:11:08 +0200

    comit_def_Add

--- Controller/TGController.py
+++ Controller/TGController.py
@@ -1,6 +1,14 @@
 from pathkib import path
+from Model.TGModel import TGModel
 
 class TGController:
     def __init__(self,model, view):
         self.model = model
         self.view = view
+        self.files = {}
+
+    def Add(self):
+        print(f'\nАнализ папки Telegram')
+        self.files = self.model.Analysis()
+        print('Анализ завершен.')
+        return self.files
