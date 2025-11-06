3df124020c863e3105c52e7201d30f0892c52acc
itought <mountyjapan24@gmail.com>
2025-11-06 13:10:03 +0200

    Added Types

--- Model/TGModel.py
+++ Model/TGModel.py
@@ -3,6 +3,9 @@ from pathlib import Path
 class TGModel:
     def __init__(self, path):
         self.path = Path(path)
-
+        self.types = {"pictures":[".png", ".jpg", ".jpeg", ".gif"],
+                      "videos":[".mp4", ".mov", ".avi", ".mkv"],
+                      "audio":[".mp3", ".wav", ".aiff"],
+                      "docs":[".docs", ".docx", ".txt", ".pdf", ".ppt", ".pptx", ".rar", ".zip"]}
 
 
