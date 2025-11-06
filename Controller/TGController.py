22b2021c777180e82dea5009cbd9c24bc5fa3490
LeoR <leorsky77@gmail.com>
2025-11-06 14:21:41 +0200

    comit_def_ShowAll

--- Controller/TGController.py
+++ Controller/TGController.py
@@ -1,5 +1,6 @@
 from pathkib import path
 from Model.TGModel import TGModel
+from Views.TGView import TGView
 
 class TGController:
     def __init__(self,model, view):
@@ -12,3 +13,18 @@ class TGController:
         self.files = self.model.Analysis()
         print('Анализ завершен.')
         return self.files
+
+    def ShowAll(self):
+        if not self.files:
+            print('Сначала Add')
+            return
+
+        path = self.view.UserPath()
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+        for key, lst in self.files.items():
+            with open(Path(path) / f'{key}.txt', 'w', encoding='utf-8') as f:
+                f.write('\n'.join(lst))
+
+        self.view.OutputResult(self.files, path)
+        return path
